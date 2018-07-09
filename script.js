27cd30b801038a753e0bd355e66b64a17da8fdad
liamfudge <liam.fudge@gmail.com>
2018-07-09 00:38:53 -0400

    first upload of files

---
+++ script.js
@@ -0,0 +1,9 @@
+// const wrapperClass = document.getElementById("pageWrapper");
+// const toggleButton = document.getElementById("toggleButton");
+
+// toggleButton.addEventListener("click", test);
+
+// function test() {
+// 	console.log("this test worked!!!!");
+// 	wrapperClass.setAttribute("class", "wrappermini");
+// }
